9e607b4f4354346f8e9ff6faa8f5d4651b05f704
new_max <qwertynestor40@gmail.com>
2017-09-01 21:23:10 +0300

    move reset css to module

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import BooksList from './components/books/List';
 import BooksSingle from './components/books/SingleItem';
+import 'reset-css'
 import './App.css';
 
 class App extends Component {
